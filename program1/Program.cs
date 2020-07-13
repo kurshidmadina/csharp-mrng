81031091131777a1707700f53ab8198b53c19c4b
kurshid madina <madinakurshied@gmail.com>
2020-07-13 21:32:01 +0530

    initial commit

---
+++ program1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahrenheit;
+
+            double celsius = 36;
+            Console.WriteLine("Celsius: " + celsius);
+
+            fahrenheit = (celsius * 9) / 5 + 32;
+            Console.WriteLine("Fahrenheit: " + fahrenheit);
+
+            Console.ReadLine();
+        }
+    }
+}
